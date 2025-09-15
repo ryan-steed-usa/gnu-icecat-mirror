c939d76c33294791cce8ce1722bd6747dadbe31f
Mark H Weaver <mhw@netris.org>
2025-09-15 14:37:18 -0400

    Update Libreddit instances in "Privacy Redirect".
    * data/extensions/{b7f9d2cd-d772-4302-8c3f-eb941af36f76}/assets/javascripts/helpers/reddit.js:
    Update list of instances based on
    <https://github.com/libreddit/libreddit-instances/blob/master/instances.md>.

--- data/extensions/{b7f9d2cd-d772-4302-8c3f-eb941af36f76}/assets/javascripts/helpers/reddit.js
+++ data/extensions/{b7f9d2cd-d772-4302-8c3f-eb941af36f76}/assets/javascripts/helpers/reddit.js
@@ -9,19 +9,9 @@ const targets = [
 const redirects = [
   // libreddit: privacy w/ modern UI
   "https://safereddit.com",
-  "https://reddit.rtrace.io",
-  "https://libreddit.privacydev.net",
-  "https://l.opnxng.com",
-  "https://libreddit.projectsegfau.lt",
-  "https://discuss.whatever.social",
-  "https://snoo.habedieeh.re",
-  "https://redlib.pussthecat.org",
-  "https://redlib.northboot.xyz",
-  "https://redlib.kylrth.com",
-  "https://red.artemislena.eu",
+  "https://lr.vern.cc",
   "https://old.reddit.com", // desktop
   "http://red.lpoaj7z2zkajuhgnlltpeqh3zyq7wk2iyeggqaduhgxhyajtdt2j7wad.onion",
-  "http://snoo.habeehrhadazsw3izbrbilqajalfyqqln54mrja3iwpqxgcuxnus7eid.onion",
 ];
 const bypassPaths = /\/(gallery\/poll\/rpan\/settings\/topics)/;
 
